89a2bd48b5d81db9b868f5a5e7c638e9ee2db5fb
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2013-02-11 17:06:45 +0100

    Import into github

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-cloudie
+Cloudie
 =======
 
 Cloudie is a simple user interface to browse Open Stack Storage and to manipulate it.
+
+(C) 2012-2013 E.Hooijmeijer and 42 B.V., [Apache 2 licensed](https://www.apache.org/licenses/LICENSE-2.0.html)
+Uses the CC-BY licensed Silk Iconset by [Mark James](http://www.famfamfam.com/)
+
+Features
+--------
+
+Based on the [JOSS](https://github.com/java-openstack/joss) library, it has the the following features:
+- creation and deletion of containers.
+- creation and deletion of stored objects.
+- down and uploading of files.
+- viewing meta-data.
+- previewing text and images.
+- opening a stored object in a public container in your browser.
+- filename based search.
+- storage of connection profiles.
+
+Usage
+-----
+
+Usage: java -jar cloudie-0.7.0-full.jar [options]
+  Options:
+    -help, -?, --?
+       Brief help.
+    -login
+       connects to the cloud. Takes 4 arguments: [AuthURL] [Tenant] [Username] [Password]
+    -profile
+       connects to the cloud using an previously stored profile.
+
+History
+=======
+
+V0.7.0 11-Feb-2013
+------------------
+- First Public Release
---
+++ src/test/java/nl/tweeenveertig/cloudie/preview/ImagePanelTest.java
@@ -0,0 +1,60 @@
+/*
+ *  Copyright 2012-2013 E.Hooijmeijer
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package nl.tweeenveertig.cloudie.preview;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ImagePanelTest {
+
+    private Graphics graphics = Mockito.mock(Graphics.class);
+
+    @Test
+    public void shouldCreate() {
+        ImagePanel panel = new ImagePanel();
+        assertEquals(new Dimension(128, 128), panel.getMinimumSize());
+    }
+
+    @Test
+    public void shouldPaintWithoutImage() {
+        ImagePanel panel = new ImagePanel();
+        panel.paintComponent(graphics);
+        //
+        Mockito.verify(graphics).fillRect(Mockito.eq(0), Mockito.eq(0), Mockito.anyInt(), Mockito.anyInt());
+    }
+
+    @Test
+    public void shouldPaintWithImage() {
+        ImagePanel panel = new ImagePanel();
+        BufferedImage img = Mockito.mock(BufferedImage.class);
+        panel.setImage(img);
+        //
+        panel.paintComponent(graphics);
+        //
+        Mockito.verify(graphics).drawImage(Mockito.eq(img), Mockito.eq(0), Mockito.eq(0), Mockito.anyInt(), Mockito.anyInt(), Mockito.eq(panel));
+        //
+        panel.clearImage();
+        panel.paintComponent(graphics);
+        //
+        Mockito.verify(graphics).fillRect(Mockito.eq(0), Mockito.eq(0), Mockito.anyInt(), Mockito.anyInt());
+    }
+}
